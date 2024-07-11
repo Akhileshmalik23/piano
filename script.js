73645a11150a7beed3d0d9a07ca40d8bf74dcd02
Akhileshmalik23 <akhileshmalik908@gmail.com>
2024-07-12 01:16:50 +0530

    make basic login

---
+++ script.js
@@ -0,0 +1,8 @@
+function playNote(note){
+    console.log("hello")
+
+    const audio = document.getElementById('piano-sound')
+    audio.src = `piano-mp3/${note}.mp3`
+    // audio.currentTime = 0; 
+    audio.play()
+}
